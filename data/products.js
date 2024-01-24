b3a86441c4527d562eaaca012e230a805341fe3d
Jessica Kent <jess@Jessicas-MacBook-Pro.local>
2024-01-24 14:51:57 +0200

    Added second image hover

--- data/products.js
+++ data/products.js
@@ -1,7 +1,7 @@
 const products = [
   {
     primaryImage: "assets/products/hiking-plain-shirt.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-t-shirt.jpeg",
     title: "Outside Vibes T-Shirt Charcoal",
     price: "59.95",
     rating: 5,
@@ -11,7 +11,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/hiking-bottle.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Hiking Bottle Outside Vibes",
     price: "39.95",
     rating: 4,
@@ -21,7 +21,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/hiking-t-shirt.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Rest in Nature T-Shirt White",
     price: "69.95",
     rating: 5,
@@ -31,7 +31,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/hiking-cap.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Outside Vibes Cap Maroon",
     price: "29.95",
     rating: 5,
@@ -41,7 +41,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/flannel-t-shirt.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Rest in Nature T-Shirt Flannel",
     price: "59.95",
     rating: 4,
@@ -51,7 +51,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/hiking-backpack.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Outside Vibes Backpack",
     price: "99.95",
     rating: 3,
@@ -61,7 +61,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/hiking-shoes.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Outside Vibes Hiking Shoes",
     price: "104.95",
     rating: 5,
@@ -71,7 +71,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/hiking-boots.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Outside Vibes Hiking Boots",
     price: "129.95",
     rating: 5,
@@ -81,7 +81,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/hiking-puffer.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Outside Vibes Puffer Jacket",
     price: "119.95",
     rating: 5,
@@ -91,7 +91,7 @@ const products = [
   },
   {
     primaryImage: "assets/products/hiking-jacket.jpeg",
-    secondaryImage: "assets/products/hiking-.jpeg",
+    secondaryImage: "assets/products/hiking-cap.jpeg",
     title: "Outside Vibes Hiking Jacket",
     price: "89.95",
     rating: 4,
