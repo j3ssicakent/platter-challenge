493612e4f82f1050c3fede56c8ebe025c70f6ce9
Jessica Kent <jess@Jessicas-MacBook-Pro.local>
2024-01-24 12:43:12 +0200

    Styled static product card

---
+++ data/products.js
@@ -0,0 +1,102 @@
+const products = [
+  {
+    primaryImage: "assets/products/hiking-plain-shirt.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Outside Vibes T-Shirt Sunshine",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: null
+  },
+  {
+    primaryImage: "assets/products/hiking-bottle.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Hike Bottle Outside Vibes Forest Green",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: 0.15
+  },
+  {
+    primaryImage: "assets/products/hiking-t-shirt.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Rest in Nature T-Shirt Charcoal",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: null
+  },
+  {
+    primaryImage: "assets/products/hiking-cap.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Outside Vibes Cap Forest Green",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: 0.15
+  },
+  {
+    primaryImage: "assets/products/flannel-t-shirt.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Rest in Nature T-Shirt Charcoal",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: 0.15
+  },
+  {
+    primaryImage: "assets/products/hiking-backpack.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Outside Vibes Backpack",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: 0.15
+  },
+  {
+    primaryImage: "assets/products/hiking-shoes.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Outside Vibes Hiking Shoes",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: 0.15
+  },
+  {
+    primaryImage: "assets/products/hiking-boots.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Outside Vibes Hiking Boots",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: 0.15
+  },
+  {
+    primaryImage: "assets/products/hiking-puffer.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Outside Vibes Puffer Jacket",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: 0.15
+  },
+  {
+    primaryImage: "assets/products/hiking-jacket.jpeg",
+    secondaryImage: "assets/products/hiking-.jpeg",
+    title: "Outside Vibes Hiking Jacket",
+    price: "104.95",
+    rating: 4,
+    reviews: 1234,
+    bestSeller: true,
+    discount: 0.15
+  },
+];
