1339a651fde74a08d095be50566df7bd4bbf275c
Jessica Kent <jess@Jessicas-MacBook-Pro.local>
2024-01-24 23:07:21 +0200

    Added second images, show more dropdown and scrollbar styling

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,12 @@
+let productHeight = document.querySelector(".product-card").offsetHeight;
+document.querySelector(".products").style.maxHeight = `${(productHeight+16)*4}px`;
 document.querySelector(".show-more").addEventListener("click", function () {
     let children = document.querySelector(".products").children;
-    for (let i = 0; i < children.length; i++) {
-        children[i].style.display = "block";
+    for (let i = 4; i < children.length; i++) {
+        children[i].style.height = `${productHeight}px`;
+        children[i].style.opacity = "1";
     }
     document.querySelector(".show-more").style.display = "none";
+    document.querySelector(".products").classList.add('open');
+    document.querySelector(".products").style.maxHeight = `${productHeight*12}px`;
 });
